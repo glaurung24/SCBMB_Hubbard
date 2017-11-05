427435e60f53623bf2d5a4815cf7a11b8bd3323f
klifab <33089791+klifab@users.noreply.github.com>
2017-11-05 11:46:19 +0100

    Update nearest_neighbours.py

--- nearest_neighbours.py
+++ nearest_neighbours.py
@@ -3,6 +3,8 @@ import numpy as np
 
 
 # doesn't work for size_x or size_y = 1
+# lists n.n. as array [[upper neighbour of N=1, left neighbour of N=1],
+#                      [u.n. N=2, l.n. N=2], ...]
 def nearest_neighbours(size_x, size_y):
     n = size_x*size_y
     out = []
