4def0119f888ef52d91aec2e9b1ae4cbd09d70b4
glaurung24 <andreas.theiler@gmx.at>
2017-11-08 12:32:24 +0100

    (at) added main.py

---
+++ main.py
@@ -0,0 +1,14 @@
+from __future__ import division
+import numpy as np
+import tools as t
+import hamiltonian
+
+
+print t.grid(2,3)
+
+#H = hamiltonian.HubbardHamiltonian(10,10)
+
+#out = H.generate_matrix()
+
+
+#print np.linalg.eig(out)
