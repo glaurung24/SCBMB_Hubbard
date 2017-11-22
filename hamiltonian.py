dd0d1a35ed86f7060ae14534014b415b7a5fa156
glaurung24 <andreas.theiler@gmx.at>
2017-11-22 13:24:25 +0100

    (at) added merge function for H_0

--- hamiltonian.py
+++ hamiltonian.py
@@ -8,12 +8,13 @@ from __future__ import division
 import tools as t
 import numpy as np
 from copy import deepcopy
+from scipy.sparse import coo_matrix
 
 
 # class that defines and generates the Hamiltonian of the system
 
 class HubbardHamiltonian:
-    def __init__(self, size_x=3, size_y=3):
+    def __init__(self, size_x=3, size_y=3, nr_up = 2, nr_down = 2):
         if(size_x <= 1):
             print 'The dimension x must be larger than 1'
             exit()
@@ -25,9 +26,9 @@ class HubbardHamiltonian:
 
         self.system_size = size_x * size_y
 
-        self.nr_spins_up = 2
-        self.nr_spins_down = 2
-        self.U = 1
+        self.nr_spins_up = nr_up
+        self.nr_spins_down = nr_down
+        self.U = 0
         self.t = 1
 
 
@@ -42,25 +43,37 @@ class HubbardHamiltonian:
         nr_configs_total = nr_configs_up * nr_configs_down
 
 
-        ##################### Calculation of H_1 ###########################################
+
+        ##################### Calculation of H_1 ####################################
 
         configs_both = []
         H_1 = np.zeros(nr_configs_total)
+        counter = 0
         for i in xrange(nr_configs_up):
             for j in xrange(nr_configs_down):
                 configs_both.append([configs_up[i], configs_down[j]])
-                H_1[i+j] = bin(configs_up[i] & configs_down[j]).count("1")
+                H_1[counter] = bin(configs_up[i] & configs_down[j]).count("1")
+                counter += 1
 
 
+#        H =
         H = np.diag(H_1*self.U) #TODO maybe switch to sparse matrix at some point?
 
+        ################ Filling H with H_0 #########################################
+
+        subH_0_up = self.generateSubH_0(configs_up)
+        subH_0_down = self.generateSubH_0(configs_down)
+
+        H_0 = self.merge_h_0(subH_0_up, subH_0_down)
+
+        H += H_0
         return H
 
-        ##################### Calculation of H_0 ###########################################
+        ##################### Calculation of H_0 ####################################
     def generateSubH_0(self, configs):
         len_bin_nr = len(bin(configs[-1]))-2
         nr_configs = len(configs)
-        H_0 = np.zeros(nr_configs, nr_configs)
+        H_0 = np.zeros([nr_configs, nr_configs])
         list_nearest_neighbors = t.nearest_neighbours(self.size_x, self.size_y)
         for ket_index in xrange(nr_configs): #TODO this part is a bit quick'n'dirty, so
                                         # maybe optimize it at some point, e.g. don't
@@ -82,7 +95,6 @@ class HubbardHamiltonian:
                                                                                 #TODO this might not be the fastest solution (use bisection search algorithm)
                         sign = (-1)**(n_annihilator+n_creator) #sign of matrix element
 
-                        print sign
                         H_0[bra_index, ket_index] = (-1)* self.t*sign
 #                            for l in xrange(nr_configs_up): #does not generate hermitian hamiltonian...
 #                                #write into the Hamiltonian, but I am not sure yet if
@@ -91,6 +103,27 @@ class HubbardHamiltonian:
 #                                H[bra_index + l*nr_configs_down, ket_index + l*nr_configs_down] = \
 #                                                                        - self.t*sign
         return H_0
+
+
+    def merge_h_0(self, h_u, h_d):
+
+            dim_up = np.shape(h_u)[0]
+            dim_down = np.shape(h_d)[0]
+            dim = dim_up * dim_down
+            h_0 = np.zeros([dim, dim])
+
+            for i in xrange(dim):
+                for j in xrange(dim):
+                    i_up = i // dim_up
+                    j_up = j // dim_up
+                    i_down = i % dim_down
+                    j_down = j % dim_down
+                    if i_up == j_up:
+                        h_0[i, j] += h_d[i_down, j_down]
+                    if i_down == j_down:
+                        h_0[i, j] += h_u[i_up, j_up]
+
+            return h_0
 
 
                     #do the jump, ask which config index k -> H_ki = -t * s
